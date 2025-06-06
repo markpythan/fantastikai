f467a064cd3947ece7a0e88e8a0fef015262e41f
markpythan <mark_pythan@keemail.me>
2025-06-06 01:56:08 -0700

    Create fantastik.py

---
+++ fantastik.py
@@ -0,0 +1,10 @@
+from seleniumbase import SB
+import time
+import requests
+import sys
+import requests
+import os
+import random
+import subprocess
+from dataclasses import dataclass
+from typing import List, Optional
